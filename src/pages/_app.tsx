cf7dd9a35f21a86eff7b6ad8cad403d6255d36d5
Oli <oliverrsaxon@gmail.com>
2022-10-05 16:57:24 +0100

    feat: google analytics component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,17 @@ import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+import Seo from '@/components/Seo';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics measurementId={process.env.NEXT_PUBLIC_MEASUREMENT_ID} />
+      <Seo />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -25,15 +24,6 @@ class MyDocument extends Document {
             crossOrigin='anonymous'
           />
         </Head>
-        <Script id='google-tag-manager' strategy='afterInteractive'>
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5WRH7DD');
-          `}
-        </Script>
         <body className='bg-zinc-100 dark:bg-zinc-800'>
           <Main />
           <NextScript />
