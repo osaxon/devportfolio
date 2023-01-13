dbc23acdc905a7e5587a151256116455ae4857e4
Oliver Saxon <oliversaxon@Olivers-MacBook-Air.local>
2023-01-13 15:49:27 +0000

    style: resized svg bg

--- components/Molecules/ParallaxBG.jsx
+++ components/Molecules/ParallaxBG.jsx
@@ -10,22 +10,22 @@ const ParallaxBG = () => {
   const rotate = useTransform(scrollYProgress, [0, 1], [0, 110], {
     clamp: false,
   });
-  const x = useTransform(scrollYProgress, [0, 1], ['0%', '90%']);
+  const x = useTransform(scrollYProgress, [0, 1], ['0px', '200px']);
   const spring = useSpring(scrollY, {
     stiffness: 150,
     damping: 15,
     mass: 2,
   });
   return (
-    <div className='absolute left-[-9rem] top-[5rem] -z-50 h-[calc(100vh*2)] w-[400px] translate-x-6 rotate-12 @lg:w-full md:top-[3rem]'>
-      <motion.div className='absolute md:right-24 lg:right-56'>
-        <SquareA className='fill-secondary stroke-base-300 opacity-50' />
+    <div className='absolute -z-50 h-[calc(100vh*2)] w-full overflow-y-hidden'>
+      <motion.div className='absolute top-10 sm:left-20 md:left-auto md:right-24 lg:right-56'>
+        <SquareA className=' fill-secondary stroke-base-300 opacity-50' />
       </motion.div>
 
       <motion.div
         style={{ translateY: spring, rotate, translateX: x }}
         transition={{ type: 'spring' }}
-        className='absolute -z-50 md:right-24 lg:right-56'
+        className='absolute top-10 -z-50 sm:left-20 md:right-24 md:left-auto lg:right-56'
       >
         <SquareB className='fill-accent stroke-base-300 opacity-50' />
       </motion.div>
