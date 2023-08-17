332428d68d988e5db55a425bb80a811e88d15934
Oliver Saxon <oliverrsaxon@gmail.com>
2023-08-17 13:23:14 +0100

    updated avatar photo

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -6,7 +6,7 @@ const Avatar = () => {
     <div className='avatar'>
       <div className='mask mask-hexagon w-32'>
         <Image
-          src='https://res.cloudinary.com/djaiep6vj/image/upload/c_thumb,w_200,g_face/v1664380136/Images/IMG_2952_ahurxs.jpg'
+          src='https://res.cloudinary.com/djaiep6vj/image/upload/v1664380757/IMG_6400_rlqvp6.jpg'
           alt='picture of Oli Saxon '
           width={200}
           height={200}
