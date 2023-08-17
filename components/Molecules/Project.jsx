423beb3d6ebe635673a8d74f115019eb2a60d066
Oliver Saxon <oliverrsaxon@gmail.com>
2023-08-17 10:32:28 +0100

    Updatated project cards

--- components/Molecules/Project.jsx
+++ components/Molecules/Project.jsx
@@ -5,19 +5,40 @@ import Link from 'next/link';
 import { LinkExternal } from '../Atoms';
 
 const Project = ({ project }) => {
-  const {name, shortDescription, description, coverImage, tags, url, content, slug} = project
+  const {
+    name,
+    shortDescription,
+    description,
+    coverImage,
+    tags,
+    url,
+    content,
+    slug,
+    moreInfo,
+  } = project;
   return (
-    <div className="card rounded-sm w-full bg-base-100 shadow-xl h-full">
-      <figure className='relative h-72'><Image src={coverImage} alt={`Cover image foblog post ${name}`} fill className='object-cover absolute' /></figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{shortDescription}</p>
-        <div className="card-actions justify-end">
-        <Link href={`/blog/${slug}`} className="btn btn-sm">More info</Link>
-        <LinkExternal href={url}>Try</LinkExternal>
+    <div className='card h-full w-full rounded-sm bg-base-100 shadow-xl'>
+      <figure className='relative h-72'>
+        <Image
+          src={coverImage}
+          alt={`Cover image foblog post ${name}`}
+          fill
+          className='absolute object-cover'
+        />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title'>{name}</h2>
+        <p>{shortDescription}</p>
+        <div className='card-actions justify-end'>
+          {moreInfo && (
+            <Link href={`/blog/${slug}`} className='btn-sm btn'>
+              More info
+            </Link>
+          )}
+          <LinkExternal href={url}>Try</LinkExternal>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
